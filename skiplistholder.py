c798353ede6ff1adb9ebf7e0d4936ba6e54768ac
Scott D <90161320+Laughing-Bulls@users.noreply.github.com>
2022-12-05 01:55:22 -0500

    Update skiplistholder.py
    Edit placeholder commands to run tests

--- skiplistholder.py
+++ skiplistholder.py
@@ -8,68 +8,57 @@ def size():
 
 def findElement(something):
     print("findElement")
-    iterations = 100 * int(math.log2(len(something)))
+    iterations = 10 * int(math.log2(something+1))
     for i in range(iterations):
         i = i + i
         i = i ^ 3
         for j in range(iterations):
             j = int(j + i)
-    print(len(something))
-    print(type(something))
     return True
 
 
-def insertElement(something):
+def insertElement(something, somethingelse):
     print("insertElement")
-    iterations = 100 * int(math.log2(len(something)))
+    iterations = 10 * int(math.log2(somethingelse+1))
     for i in range(iterations):
         i = i + i
         i = i - 20
-        i = i ^ 3
         for j in range(iterations):
-            j = int(j + i)
-    print(len(something))
-    print(type(something))
+            j = int(i)
     return True
 
 
 def removeElement(something):
     print("removeElement")
-    iterations = 100 * int(math.log2(len(something)))
+    iterations = 10 * int(math.log2(something+1))
     for i in range(iterations):
         i = i + i
         i = i * i
         i = i ^ 3
         for j in range(iterations):
             j = int(j + i)
-    print(len(something))
-    print(type(something))
     return True
 
 
 def closestKeyAfter(something):
     print("closestKeyAfter")
-    iterations = 100 * int(math.log2(len(something)))
+    iterations = 10 * int(math.log2(something+1))
     for i in range(iterations):
         i = i + i
         i = i ^ 3
         i = i / (i+1)
         for j in range(iterations):
             j = int(j + i)
-    print(len(something))
-    print(type(something))
     return True
 
 
 def closestKeyBefore(something):
     print("closestKeyBefore")
-    iterations = 100 * int(math.log2(len(something)))
+    iterations = 10 * int(math.log2(something+1))
     for i in range(iterations):
         i = i + i
         i = i ^ 3
         i = i % (i+1) + (i*2)
         for j in range(iterations):
             j = int(j + i)
-    print(len(something))
-    print(type(something))
     return True
