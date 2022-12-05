9ed23ebaaf17f02b1e0fc395ae2dda7d2a04fe95
Ian Mutawa <mma47@njit.edu>
2022-12-05 14:29:18 -0500

    Add the logic for  method

--- SkipList.py
+++ SkipList.py
@@ -1,4 +1,5 @@
 import math
+import random
 
 # TODO: Add documentation to each method
 
@@ -20,8 +21,17 @@ class SkipList:
     self.length = 0
 
   # insert/replace value
-  def insertElement(key, value):
-    pass
+  def insertElement(self, key, value):
+    pointer = self.findElement(key)
+    element = self.__insertAfterAbove(pointer, None, key, value)
+
+    while random.random() > 0.5:
+      if pointer.above == None:
+        pointer = p.before
+
+      pointer = pointer.above
+      element = self.__insertAfterAbove(pointer, element, key, value)
+
 
   def removeElement(self, key):
     pass
@@ -58,19 +68,36 @@ class SkipList:
     lastLevel = len(self.levels) - 1
     return self.levels[lastLevel][0]
 
+  def __insertAfterAbove(self, after, above, key, value):
+    node = SkipListNode()
+
+    node.key   = key
+    node.value = value
+
+    if after != None:
+      node.after         = after.after
+      after.after.before = node
+      after.after        = node
+
+    if above != None:
+      node.above  = above
+      node.below  = above.above
+      above.above = node
+
   def __insertLevelAbove(self, firstKey, LastKey):
     plusInfinity  = SkipListNode()
     minusInfinity = SkipListNode()
 
-    minusInfinity.key = -math.inf
-    minusInfinity.after   = plusInfinity
-    minusInfinity.above   = firstKey
+    minusInfinity.key   = -math.inf
+    minusInfinity.after = plusInfinity
+    minusInfinity.above = firstKey
 
-    plusInfinity.key = math.inf
-    plusInfinity.before  = minusInfinity
-    plusInfinity.above   = LastKey
+    plusInfinity.key    = math.inf
+    plusInfinity.before = minusInfinity
+    plusInfinity.above  = LastKey
 
     return [minusInfinity, plusInfinity]
 
 spl = SkipList()
+spl.insertElement(1, 'label - 1')
 spl.display()
